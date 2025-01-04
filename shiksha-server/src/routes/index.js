f394c4db1747ab82f28206ec670cbd1ff74a8297
Anshul <you@example.com>
2025-01-04 19:02:03 +0530

    feat: add image route for serving uploaded images

--- shiksha-server/src/routes/index.js
+++ shiksha-server/src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
 import v1Routes from './v1/index.js';
+import ImageRoute from './imageRoute.js';
 
 const router = express.Router();
 
 router.use('/v1', v1Routes);
+router.use('/uploads', ImageRoute);
 
 export default router;
