a0c8c14c8d0c38108e95e2b6a6231d0d8599848a
developer-divyang <divyangdeveloper@gmail.com>
2025-03-01 11:04:26 +0530

    set from live

--- eduroutez-next/app/dashboard/answer/_components/question-answer-tables/cell-action.tsx
+++ eduroutez-next/app/dashboard/answer/_components/question-answer-tables/cell-action.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 'use client';
 import { AlertModal } from '@/components/modal/alert-modal';
 import { Button } from '@/components/ui/button';
@@ -39,7 +40,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['question-answers'] });
-      router.push('/dashboard/question-answer');
+      router.push('/dashboard/answer');
     },
     onSettled: () => {
       setOpen(false);
@@ -92,3 +93,99 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
     </>
   );
 };
+=======
+'use client';
+import { AlertModal } from '@/components/modal/alert-modal';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import axiosInstance from '@/lib/axios';
+import { Answer } from '@/types';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { Edit, Eye, MoreHorizontal, Trash } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+interface CellActionProps {
+  data:Answer;
+}
+
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  const queryClient = useQueryClient();
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  const deleteQuestionAnswerMutation = useMutation({
+    mutationFn: async (questionAnswerId: string) => {
+      const response = await axiosInstance({
+        url: `${apiUrl}/question-answer/${questionAnswerId}`,
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      return response;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['question-answers'] });
+      router.push('/dashboard/answer');
+    },
+    onSettled: () => {
+      setOpen(false);
+      setLoading(false);
+    }
+  });
+
+  const onConfirm = async () => {
+    setLoading(true);
+    deleteQuestionAnswerMutation.mutate(data._id);
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onConfirm}
+        loading={loading}
+      />
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+
+            <DropdownMenuItem
+            onClick={() =>
+              router.push(`https://eduroutez.com/question-&-answers`)
+            }
+            >
+            <Eye className="mr-2 h-4 w-4" /> View Q/A 
+            </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            <Trash className="mr-2 h-4 w-4" /> Delete
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() =>
+              router.push(`/dashboard/answer/update/${data._id}/`)
+            }
+          >
+            <Edit className="mr-2 h-4 w-4" /> Answer
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
+>>>>>>> 6d90c88e8ac83421033a208f10e92fee37b54c65
--- eduroutez-next/app/dashboard/question-answer/_components/question-answer-tables/cell-action.tsx
+++ eduroutez-next/app/dashboard/question-answer/_components/question-answer-tables/cell-action.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 'use client';
 import { AlertModal } from '@/components/modal/alert-modal';
 import { Button } from '@/components/ui/button';
@@ -42,7 +43,7 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['question-answers'] });
       toast.success('FAQ deleted successfully');
-      router.push('/dashboard/answer');
+      router.push('/dashboard/question-answer');
     },
     onSettled: () => {
       setOpen(false);
@@ -88,3 +89,95 @@ export const CellAction: React.FC<CellActionProps> = ({ data }) => {
     </>
   );
 };
+=======
+'use client';
+import { AlertModal } from '@/components/modal/alert-modal';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import axiosInstance from '@/lib/axios';
+import { QuestionAnswer } from '@/types';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { Edit, MoreHorizontal, Trash } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { toast } from 'sonner';
+
+interface CellActionProps {
+  data: QuestionAnswer;
+}
+
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  const queryClient = useQueryClient();
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  const deleteQuestionAnswerMutation = useMutation({
+    mutationFn: async (questionAnswerId: string) => {
+      console.log('questionAnswerId', questionAnswerId);
+      const response = await axiosInstance({
+        url: `${apiUrl}//faq/${questionAnswerId}`,
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      return response;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['question-answers'] });
+      toast.success('FAQ deleted successfully');
+      router.push('/dashboard/question-answer');
+    },
+    onSettled: () => {
+      setOpen(false);
+      setLoading(false);
+    }
+  });
+
+  const onConfirm = async () => {
+      setLoading(true);
+      await deleteQuestionAnswerMutation.mutateAsync(data._id);
+    };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onConfirm}
+        loading={loading}
+      />
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+
+          <DropdownMenuItem
+            onClick={() =>
+              router.push(`/dashboard/question-answer/update/${data._id}/`)
+            }
+          >
+            <Edit className="mr-2 h-4 w-4" /> Update
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            <Trash className="mr-2 h-4 w-4" /> Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
+>>>>>>> 6d90c88e8ac83421033a208f10e92fee37b54c65
