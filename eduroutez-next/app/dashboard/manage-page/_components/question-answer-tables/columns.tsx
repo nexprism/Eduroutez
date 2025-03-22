d34b5877cf745418c343f57edfbc5210dd98102e
Anshul <you@example.com>
2025-03-22 11:28:17 +0530

    feat: add image handling and improve question-answer listing pagination

--- eduroutez-next/app/dashboard/manage-page/_components/question-answer-tables/columns.tsx
+++ eduroutez-next/app/dashboard/manage-page/_components/question-answer-tables/columns.tsx
@@ -6,24 +6,35 @@ import { CellAction } from './cell-action';
 import { Badge } from '@/components/ui/badge';
 import { ManagePages } from '@/types';
 
+const IMAGE_URL = process.env.NEXT_PUBLIC_IMAGES;
+
 export const columns: ColumnDef<ManagePages>[] = [
   {
     header: 'ID',
-    cell: ({ row }) => <div>{row.index + 1}</div>, // Corrected formatting
+    cell: ({ row }) => <div>{row.index + 1}</div>,
   },
   {
+    accessorKey: 'title',
     header: 'Title',
-    accessorKey: 'question', // Simplified with accessorKey
     cell: ({ row }) => <div>{row.original.title}</div>,
   },
   {
+    accessorKey: 'description',
     header: 'Description',
-    accessorKey: 'answer', // Added accessorKey for consistency
     cell: ({ row }) => (
-      <div
-        dangerouslySetInnerHTML={{ __html: row.original.description || '' }} // Fixed syntax
+      <div 
+        dangerouslySetInnerHTML={{ 
+          __html: row.original.description 
+            ? row.original.description.substring(0, 100) + (row.original.description.length > 100 ? '...' : '')
+            : '' 
+        }}
       />
     ),
+  },
+
+  {
+    header: 'Images',
+    cell: ({ row }) => <img className='w-[2.5rem] h-[2rem] rounded-full' src={`${IMAGE_URL}/${row.original.image}`} alt="Icon" />
   },
   {
     accessorKey: 'status',
@@ -32,7 +43,7 @@ export const columns: ColumnDef<ManagePages>[] = [
       <div className="flex w-32 space-x-1">
         <Badge
           variant={!row.original.status ? 'secondary' : 'default'}
-          className="text-xs "
+          className="text-xs"
         >
           {row.original.status ? 'Active' : 'Inactive'}
         </Badge>
@@ -41,7 +52,7 @@ export const columns: ColumnDef<ManagePages>[] = [
   },
   {
     id: 'actions',
-    header: 'Actions', // Added header for clarity
+    header: 'Actions',
     cell: ({ row }) => <CellAction data={row.original} />,
   },
 ];
